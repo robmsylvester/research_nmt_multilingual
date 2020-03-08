cc2afe762a282510ec2c7df64599b88de50195a6
Rob Sylvester <Rob.Sylvester@logmein.com>
2020-03-08 12:17:27 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Multilingual NMT Research
+
+## Purpose
+
+This library is aimed at implementing research ideas for low-resource multilingual seq2seq NMT architectures.
+
+## Installation and Preparation
+
+1. Clone this repository.
+
+2. Create a virtual environment. This code was run using python 3.6.
+
+3. Install the library locally
+   `pip install -r requirements.txt`
+   `python setup.py install`
+
+## Usage
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="research_nmt_multilingual",
+    version='0.0.1',
+    author="Rob Sylvester",
+    author_email="robmsylvester@gmail.com",
+    description="This research repo contains research experiments around Multilingual seq2seq neural architectures",
+    download_url='',
+    license="Proprietary",
+    python_requires='=3.6',
+    packages=find_packages(),
+)
